4a35eb34f65838befb5886f1fcbcc7cd9af34088
abbosmamasodirov <mamasodirovabbos@gmai.com>
2022-11-27 00:31:04 +0500

    Initial commit

---
+++ src/main/java/uz/spring/mvc_market/MvcMarketApplication.java
@@ -0,0 +1,14 @@
+package uz.spring.mvc_market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+public class MvcMarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MvcMarketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uz/spring/mvc_market/MvcMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.spring.mvc_market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcMarketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
